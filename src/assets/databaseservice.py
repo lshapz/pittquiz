946469a2a4a89682b6b5f06062fd19760aff4262
lshapz <laurashapiro01@gmail.com>
2017-05-10 18:48:55 -0400

    backend service almost done

--- src/assets/databaseservice.py
+++ src/assets/databaseservice.py
@@ -2,4 +2,8 @@ import requests
 import json
 
 actor = input("who do you want to search?")
-r = requests.get("")
+r = requests.get("https://api.themoviedb.org/3/search/person?api_key=0565cf9b565d67438edd46ccfecb5c1d&language=en-US&query=" + actor + "&page=1&include_adult=false")
+
+print(r.json())
+
+# https://www.google.com/search?q=export+python+to+js&oq=export+python+to+js&aqs=chrome..69i57j0l5.2290j0j7&sourceid=chrome&ie=UTF-8#q=export+python+json+to+use+n+js
